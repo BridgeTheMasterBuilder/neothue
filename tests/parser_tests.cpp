f7659068e7eec9852c8e711db328a41a48a9706f
Master Builder <gylfi@stebbi.is>
2022-11-07 16:21:29 +0000

    BUGFIX: Use of #include in parser_tests.cpp to inline all the test cases in the source file leads to excessive memory usage; replaced it with code that reads the test cases from the file

--- tests/parser_tests.cpp
+++ tests/parser_tests.cpp
@@ -1,6 +1,8 @@
+#include "../src/util.h"
 #include "Lexer.h"
 #include "Parser.h"
 #include <boost/ut.hpp>
+#include <fstream>
 #include <string>
 #include <vector>
 
@@ -34,9 +36,13 @@ void test(const std::string item, const std::string message, const bool expected
 
 int main()
 {
-  const std::vector<std::string> legal_programs = {
-#include "testdata"
-  };
+  std::vector<std::string> legal_programs;
+
+  std::ifstream testdata("../tests/testdata");
+  if (!testdata) throw File_not_found("../tests/testdata");
+
+  for (std::string testcase; std::getline(testdata, testcase, '\n');)
+    legal_programs.emplace_back(testcase);
 
   for (const auto& program : legal_programs) {
     test(program, "\nParser did not recognize:\n\t" + program + "\nas a valid program", true);
